5ec21406ff254a71766844d56f5e937a855e396b
aleshtimur <76618163+aleshtimur@users.noreply.github.com>
2020-12-27 16:28:50 +0300

    Update Hello 1.py

--- Hello 1.py
+++ Hello 1.py
@@ -1,8 +1,9 @@
-print("Hello,user!")
-user_name = input("What is your name?")
-print("Hello, my name is Van, I'm artist, I'm performance artist.")
-print("Hello", user_name)
-
-user_year = int(input('когда ты родился'))
-yearsold = 2020 - user_year
-print(yearsold)
+print('Hello, user!')
+user_name = input("What is ur name?")
+print('Hello', user_name, "in ur name", len(user_name), 'letters')
+user_year = int(input('When u was born?'))
+if len(str(user_year)) == 4:
+    print("nice year", user_year)
+    print(2020-user_year)
+else:
+    print("U make a little mistake")
