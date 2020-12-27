8ddaae3d22ae57914108299d4daa3db483c15957
aleshtimur <76618163+aleshtimur@users.noreply.github.com>
2020-12-27 18:47:20 +0300

    Update 04 пазл.py

--- 04 пазл.py
+++ 04 пазл.py
@@ -4,9 +4,10 @@ while isGuessed != True:
     puzzle = "\nБыло 2 козла, сколько? \nНа размышление дается 30 секунд"
     answer = input(puzzle)
     if answer == "2":
-        print('Пойман за руку как дешевка.')
-    elif answer == "шайбу карге":
-        print("Правильно!")
+        print('Есть пробитие!')
+        isGuessed = True
+    elif answer == "снюс":
+        print("Шайбу карге!")
     else:
         print("Пробуй еще")
     print("пасибо за игру")
