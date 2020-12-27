89a66e0d8d34c5fe30e14daef93d742ef77f4156
aleshtimur <76618163+aleshtimur@users.noreply.github.com>
2020-12-27 15:32:53 +0300

    Update Hello 1.py

--- Hello 1.py
+++ Hello 1.py
@@ -2,3 +2,7 @@ print("Hello,user!")
 user_name = input("What is your name?")
 print("Hello, my name is Van, I'm artist, I'm performance artist.")
 print("Hello", user_name)
+
+user_year = int(input('когда ты родился'))
+yearsold = 2020 - user_year
+print(yearsold)
