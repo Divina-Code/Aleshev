1ca81a45a0e242530d36b7b482857c590718fe16
aleshtimur <76618163+aleshtimur@users.noreply.github.com>
2020-12-28 14:46:58 +0300

    Update 05 угадай чалсо.py

--- 05 угадай чалсо.py
+++ 05 угадай чалсо.py
@@ -1,11 +1,17 @@
-chislo = False
 
+number1 = int(228)
+chislo = False
 while chislo != True:
-    number =input('Введите ваше число')
-    if number == '228':
+    number =int(input('Введите ваше число'))
+    if number == number1:
         chislo = True
         print('Правильно!')
-
+    elif number > number1:
+        print('Меньше!')
+        chislo = False
+    elif number < number1:
+        print('Больше!')
+        chislo = False
     else:
         chislo =False
         print('Пробуй еще!')
