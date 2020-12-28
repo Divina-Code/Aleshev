25413dc1f06b529b8d100ea8d1ff9883f6b10752
aleshtimur <76618163+aleshtimur@users.noreply.github.com>
2020-12-28 14:26:55 +0300

    Create 05 угадай чалсо.py

---
+++ 05 угадай чалсо.py
@@ -0,0 +1,12 @@
+chislo = False
+
+while chislo != True:
+    number =input('Введите ваше число')
+    if number == '228':
+        chislo = True
+        print('Правильно!')
+
+    else:
+        chislo =False
+        print('Пробуй еще!')
+print('Списание со счета: 10000 рублей')
